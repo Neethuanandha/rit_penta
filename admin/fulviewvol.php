6a4cb8cfe673e931f63b3a384e8584469734f5b7
Neethuanandha <44440215+Neethuanandha@users.noreply.github.com>
2018-10-24 20:32:31 +0530

    first commit

---
+++ admin/fulviewvol.php
@@ -0,0 +1,165 @@
+<?php
+
+include_once('../global.php'); ?>
+<?php include_once('../root/functions.php'); ?>
+<?php  
+
+auth_login();
+
+include_once('includes/header.php'); ?>
+
+
+
+
+<!doctype html>
+<html lang="en">
+  <head>
+<link rel="icon" href="assets/image/favicon/favicon.ico">
+
+ 
+  <link rel="stylesheet" href="assets/css/style.css">  
+
+  
+</head>
+
+<body>
+	<section>
+
+<div class="page-header">
+	<div class="h3 mb-3 bg-primary text-white"><h1> Complete Details</h1>
+		</div></div>
+
+<div class="row">
+	
+	<div class="col-12">
+		
+
+
+
+<table class="table table-hover bg-white">
+  <thead>
+    <tr>
+      <th scope="col">Volunteer Id</th>
+      <td>
+      </td>
+    </tr>
+    <tr>
+      <th scope="col">Admission Number</th>
+      <td></td>
+    </tr>
+    <tr>
+      <th scope="col">Name</th>
+      <td></td>
+    </tr>
+    <tr>
+      <th scope="col">DOB</th>
+      <td></td>
+    </tr>
+    <tr>
+      <th scope="col">Gender</th>
+      <td></td>
+    </tr>
+    <tr>
+      <th scope="col">Religion</th>
+      <td></td>
+    </tr>
+    <tr>
+      <th scope="col">Caste</th>
+      <td></td>
+    </tr>
+
+      <tr>
+
+      <th scope="col">Address</th>
+      <td></td>
+    </tr>
+    <tr>
+      <th scope="col">Year of Admission</th>
+      <td></td>
+    </tr>
+<tr>
+      <th scope="col">Course</th>
+      <td></td>
+    </tr>
+    <tr>
+      <th scope="col">Branch</th>
+      <td></td>
+    </tr>
+    <tr>
+      <th scope="col">Semester</th>
+      <td></td>
+    </tr>
+    <tr>
+      <th scope="col">Department</th>
+    </tr>
+    <tr>
+      <th scope="col">Blood Group</th>
+    </tr>
+    <tr>
+      <th scope="col">Mobile Number</th>
+    </tr>
+    <tr>
+      <th scope="col">Alternate Mobile Number</th>
+    </tr>
+    <tr>
+      <th scope="col">Email Id</th>
+      <th scope="col"></th>
+    </tr>
+  </thead>
+     
+     <tbody>
+      <tr>
+      	<td>101</td>
+      	<td>16MP675</td>
+      	<td>NEETHU A</td>
+        <td>14-11-1995</td>
+        <td>Female</td>
+        <td>Hindu</td>
+        <td>Nair</td>
+        <td>AmbiyilKizhakkathil,Ayikkunnam,Sasthamcotta po,kollam,pin:690521</td>
+        <td>2016</td>
+        <td>MCA</td>
+        <td>Computer Application</td>
+        <td>5</td>
+      	<td>MCA</td>
+        <td>9567140622</td>
+        <td>9995520476</td>
+      	<td>neethunithin68@gmail.com</td>
+
+
+    </tr>
+
+  </tbody>
+</table>
+</div>
+</div>
+</section>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+	</div>
+
+
+</div>
+
+
+
+
+
+
+
+
+
+			
+
---
+++ admin/viewvol.php
@@ -0,0 +1,99 @@
+<?php
+
+include_once('../global.php'); ?>
+<?php include_once('../root/functions.php'); ?>
+<?php  
+
+auth_login();
+
+include_once('includes/header.php'); ?>
+
+
+
+
+<!doctype html>
+<html lang="en">
+  <head>
+<link rel="icon" href="assets/image/favicon/favicon.ico">
+
+ 
+  <link rel="stylesheet" href="assets/css/style.css">  
+
+  
+</head>
+
+<body>
+	<section>
+
+<div class="page-header">
+	<div class="h3 mb-3 bg-primary text-white"><h1>Volunteer Details</h1>
+		</div></div>
+
+<div class="row">
+	
+	<div class="col-12">
+		
+
+
+
+<table class="table table-hover bg-white">
+  <thead>
+    <tr>
+      <th scope="col">Volunteer Id</th>
+      <th scope="col">Admission Number</th>
+      <th scope="col">Name</th>
+      <th scope="col">Department</th>
+      <th scope="col">Mobile Number</th>
+      <th scope="col">Email Id</th>
+      <th scope="col"></th>
+    </tr>
+  </thead>
+     
+     <tbody>
+      <tr>
+      	<td>101</td>
+      	<td>16MP675</td>
+      	<td>NEETHU A</td>
+      	<td>MCA</td>
+      	<td>9567140622</td>
+      	<td>neethunithin68@gmail.com</td>
+
+
+<td><a href="admin/fulviewvol.php?id=12&" class="btn btn-primary btn-sm">view</a>
+      </td>
+    </tr>
+
+  </tbody>
+</table>
+</div>
+</div>
+</section>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+	</div>
+
+
+</div>
+
+
+
+
+
+
+
+
+
+			
+
